f55530aa79ae5dd367db9d9de0ab56a6f00b983b
javiluna84 <javier.luna.jrlf@gmail.com>
2023-01-03 09:54:38 -0500

    init

---
+++ flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/") #Decorator 
+def head():
+    return "Hello World!"
+
+@app.route("/second")
+def second():
+    name= "Elyes"
+    return f"My name is {name}"
+
+@app.route("/third/subthird")
+def third():
+    return "Returned from subthird endpoint!"
+
+@app.route('/forth/<string:name>')
+def forth(name):
+    return f"Hello it's {name} again!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=3000,debug=False)
